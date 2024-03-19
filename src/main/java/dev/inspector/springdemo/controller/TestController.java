e1c27c2dd1a6767e56dca2b09d2486dd3a382fbb
simone.spinelli <simone.spinelli@parsec326.it>
2024-03-19 17:25:49 +0100

    Aggiunta dell'endpoint di test e della lista di path nell'application.yaml

--- src/main/java/dev/inspector/springdemo/controller/TestController.java
+++ src/main/java/dev/inspector/springdemo/controller/TestController.java
@@ -3,10 +3,16 @@ package dev.inspector.springdemo.controller;
 import dev.inspector.springdemo.entity.User;
 import dev.inspector.springdemo.service.TestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest")
@@ -19,5 +25,10 @@ public class TestController {
     User test(@PathVariable String name) {
         System.out.println("REST request received.");
         return testServiceImpl.findUser(name);
+    }
+
+    @GetMapping("")
+    ResponseEntity<String> test() {
+       return ResponseEntity.ok("Hello");
     }
 }
