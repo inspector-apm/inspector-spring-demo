ca734891a7ca4f75d249ef4781046cba49eeb8df
grove693 <anghel.aranyos@yahoo.com>
2024-12-20 19:36:07 +0200

    Add javadoc to RestTemplate creation

--- src/main/java/dev/inspector/springdemo/config/AppConfig.java
+++ src/main/java/dev/inspector/springdemo/config/AppConfig.java
@@ -13,6 +13,12 @@ public class AppConfig {
     @Autowired
     private RestTemplateMonitoringInterceptor restTemplateInterceptor;
 
+    /**
+     * Method that creates a RestTemplate bean.
+     * In order to enable monitoring for it, we have to explicitly add {@link RestTemplateMonitoringInterceptor} as an interceptor
+     * @param builder rest template builder
+     * @return the rest template instance used for sending http requests
+     */
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder
